f66901b1d0b505f1ee2b0202d39cd4ed160f5c01
Ademas <donatelodiman@gmail.com>
2024-03-18 00:43:39 +0300

    complete V1.01

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -15,17 +15,17 @@ public class Main {
         Connection connection = getConnection();
         UserServiceImpl userService = new UserServiceImpl();
 
-//        userService.createUsersTable();
-//
-//        userService.saveUser("Name1", "LastName1", (byte) 20);
-//        userService.saveUser("Name2", "LastName2", (byte) 25);
-//        userService.saveUser("Name3", "LastName3", (byte) 31);
-//        userService.saveUser("Name4", "LastName4", (byte) 38);
+        userService.createUsersTable();
 
-//        userService.removeUserById(1);
-//        System.out.println(userService.getAllUsers().toString());
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+
+        userService.removeUserById(1);
+        System.out.println(userService.getAllUsers().toString());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
         // реализуйте алгоритм здесь
     }
 }
